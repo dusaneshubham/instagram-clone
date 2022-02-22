24942fd025e9e4582a4247ec45c6c8be0d200c9b
Ankit Verma <ankitverma209171@gmail.com>
2022-02-22 17:55:14 +0530

    angular ui routing configured

--- client/app.js
+++ client/app.js
@@ -3,19 +3,43 @@
 const app = angular.module("instagramApp", ["ui.router"]);
 
 app.config(function ($stateProvider) {
-    const loginState = {
-        name: "login",
+    const indexState = {
+        name: "index",
         url: "",
         templateUrl: "views/login.html",
     };
+    $stateProvider.state(indexState);
+
+    const loginState = {
+        name: "login",
+        url: "/login",
+        templateUrl: "views/login.html",
+    };
+    $stateProvider.state(loginState);
+
+    const signUpState = {
+        name: "signup",
+        url: "/signup",
+        templateUrl: "views/signup.html",
+    };
+    $stateProvider.state(signUpState);
 
     const homeState = {
         name: "home",
         url: "/home",
         templateUrl: "views/home.html",
     };
-
-    $stateProvider.state("otherwise", { url: "/" });
-    $stateProvider.state(loginState);
     $stateProvider.state(homeState);
+
+    const profileState = {
+        name: "profile",
+        url: "/profile",
+        templateUrl: "views/profile.html",
+    };
+    $stateProvider.state(profileState);
+
+    $stateProvider.state("otherwise", {
+        url: "*path",
+        templateUrl: "views/error-not-found.html",
+    });
 });
